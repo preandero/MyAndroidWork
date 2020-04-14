7958df58594723aed5fe0a91df42cc9dafde0eae
preandero <preandero@gmail.com>
2020-04-14 11:05:08 +0900

    학원커밋 3-1

--- a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
+++ a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.linear4);
 
         int ss = 100;
-        String tt = "하하";
+        String tt = "하하호호";
     }
 }
